8b43839279ad5999300a2de56d78b61e32a22d75
Sushant Thyagaraj <sushantt@gmail.com>
2023-03-16 18:52:09 -0700

    Implemented waiting room logic on backend. Rooms are generated every 10 seconds, and room sends a game if multiple clients join in that period.

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import { v4 } from "uuid";
 import { fetchWord } from "./services";
+import cors from "cors";
+
+// needed to bypass CORS issues
 const app = express();
+app.use(cors());
 
 // get room for players
 
@@ -9,7 +13,13 @@ let rooms: any = {};
 let currentRoom = v4();
 
 app.get("/room", async (req, res) => {
-  rooms[currentRoom].members.add(req.ip);
+  if (!rooms[currentRoom]) {
+    res.status(204).end();
+    return;
+  }
+
+  // using client-side session ID to differentiate between players
+  rooms[currentRoom].members.add(req.query.sid);
   if (rooms[currentRoom].members.size > 1) {
     res
       .status(200)
@@ -34,18 +44,25 @@ async function assignRooms() {
   return roomId;
 }
 
+var priorRoom = "";
+async function manageRooms() {
+  if (priorRoom && rooms[priorRoom] && rooms[priorRoom].members < 2) {
+    delete rooms[priorRoom];
+  }
+  priorRoom = await assignRooms();
+  setTimeout(manageRooms, 10000);
+}
+
 function main() {
-  const port = 3000;
+  const port = 3001;
   app.listen(port, () => {
     console.log(`Backend listening on port ${port}`);
   });
-  var priorRoom = "";
-  setInterval(async () => {
-    if (priorRoom && rooms[priorRoom].members < 2) {
-      delete rooms[priorRoom];
-    }
-    priorRoom = await assignRooms();
-  }, 10000);
+
+  manageRooms();
+  setInterval(() => {
+    console.log(rooms);
+  }, 1000);
 }
 
 main();
